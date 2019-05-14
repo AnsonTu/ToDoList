f9d53b89cd5f50de3a753782c571a02156b0b5f6
Anson Tu <anson9137@gmail.com>
2019-05-14 16:52:29 -0400

    Separated unfinished and finished items into two lists

--- src/ShowItems.js
+++ src/ShowItems.js
@@ -2,13 +2,31 @@ import React from "react";
 
 class ShowItems extends React.Component {
   render() {
+    let finishedItems = this.props.listOfItems.filter(items => items.completed === true);
+    let unfinishedItems = this.props.listOfItems.filter(items => items.completed === false);
     return (
       <div>
-        <ul className="to-do-list">
-          {this.props.listOfItems.map(item => (
+        <ul className="unfinished-items-list">Unfinished Items
+          {unfinishedItems.map(item => (
             <li key={item.id} className="list-item">
               <div className="list-item-details">
-                <input type="checkbox" value={item.completed} onClick={() => this.props.onChangeCheckbox(item.id)}/>
+                <input type="checkbox" checked={item.completed} onClick={() => this.props.onChangeCheckbox(item.id)}/>
+                {item.message}
+                <button
+                  onClick={() => this.props.onDeleteItem(item)}
+                  className="remove-item"
+                >
+                  Remove
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <ul className="finished-items-list">Finished Items
+          {finishedItems.map(item => (
+            <li key={item.id} className="list-item">
+              <div className="list-item-details">
+                <input type="checkbox" checked={item.completed} onClick={() => this.props.onChangeCheckbox(item.id)}/>
                 {item.message}
                 <button
                   onClick={() => this.props.onDeleteItem(item)}
